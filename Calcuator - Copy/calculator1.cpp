553ee7586ee60d676e60b96ffcf4dc797a8e3dc8
Vahan <88747696+vkhachatrian@users.noreply.github.com>
2022-03-06 11:15:12 +0400

    Add files via upload

---
+++ Calcuator - Copy/calculator1.cpp
@@ -0,0 +1,14 @@
+//#include <QtWidgets>
+//
+//
+//int main(int argc,char**argv)
+//{
+//    QApplication app(argc,argv);
+//    QWidget wgt;
+//    QPixmap pix("C:\\Projects\\Test\\clock.jpg");
+//    QCursor cur(pix);
+//    wgt.setCursor(cur);
+//    wgt.resize(180,100);
+//    wgt.show();
+//    return app.exec();
+//}
